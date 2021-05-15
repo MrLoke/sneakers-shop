f44a2735b23a76ee115472459cbf53009c78b225
Michał Grzesiak <darkjjookkeerr@gmail.com>
2021-05-15 16:03:34 +0200

    Add app routes

---
+++ src/routes/Routes.js
@@ -0,0 +1,35 @@
+import { lazy, Suspense } from 'react'
+import Navbar from 'components/Navbar/Navbar'
+import MobileMenu from 'components/MobileMenu/MobileMenu'
+import LoadingSpinner from 'components/LoadingSpinner/LoadingSpinner'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import UserCart from 'components/UserCart/UserCart'
+
+const HomePage = lazy(() => import('pages/HomePage/HomePage'))
+const CartPage = lazy(() => import('pages/CartPage/CartPage'))
+const ProductPage = lazy(() => import('pages/ProductPage/ProductPage'))
+const LoginPage = lazy(() => import('pages/LoginPage/LoginPage'))
+const RegisterPage = lazy(() => import('pages/RegisterPage/RegisterPage'))
+const NotFound = lazy(() => import('pages/NotFound/NotFound'))
+
+const Routes = () => {
+  return (
+    <Router>
+      <Suspense fallback={<LoadingSpinner fullScreen />}>
+        <Navbar />
+        <MobileMenu />
+        <UserCart />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/cart' component={CartPage} />
+          <Route exact path='/product/:name' component={ProductPage} />
+          <Route exact path='/login' component={LoginPage} />
+          <Route exact path='/signup' component={RegisterPage} />
+          <Route exact path='*' component={NotFound} />
+        </Switch>
+      </Suspense>
+    </Router>
+  )
+}
+
+export default Routes
