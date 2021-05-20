7ba45668044cd8e3e2190bf2f3610a9171ab1932
Michał Grzesiak <darkjjookkeerr@gmail.com>
2021-05-20 18:00:07 +0200

    Create and add firebase logic to register new user

---
+++ src/pages/RegisterPage/RegisterPage.js
@@ -0,0 +1,12 @@
+import SignupForm from 'components/SignupForm/SignupForm'
+import { Wrapper } from './RegisterPageStyled'
+
+const RegisterPage = () => {
+  return (
+    <Wrapper>
+      <SignupForm />
+    </Wrapper>
+  )
+}
+
+export default RegisterPage
