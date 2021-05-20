9504c09950afad4ac3aad841a6cd8db086809ae7
Michał Grzesiak <darkjjookkeerr@gmail.com>
2021-05-20 22:08:52 +0200

    Add redirect to home page if user is logged

--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,10 +1,19 @@
 import SignupForm from 'components/SignupForm/SignupForm'
+import { selectUser } from 'features/userSlice'
+import { useSelector } from 'react-redux'
+import { Redirect } from 'react-router'
 import { Wrapper } from './RegisterPageStyled'
 
 const RegisterPage = () => {
+  const user = useSelector(selectUser)
+
   return (
     <Wrapper>
-      <SignupForm />
+      {!user.isLogged ? (
+        <SignupForm />
+      ): (
+        <Redirect to='/'/>
+      )}
     </Wrapper>
   )
 }
