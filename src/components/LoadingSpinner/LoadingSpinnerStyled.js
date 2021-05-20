69e5537c576e4fb91a96768d94dc049017420cdc
Michał Grzesiak <darkjjookkeerr@gmail.com>
2021-05-20 18:18:55 +0200

    Create and style loading spinner

---
+++ src/components/LoadingSpinner/LoadingSpinnerStyled.js
@@ -0,0 +1,44 @@
+import styled from 'styled-components/macro'
+
+export const StyledSpinnerContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: ${({ fullScreen }) => (fullScreen ? '100vh' : 'auto')};
+  background-color: ${({ fullScreen, theme }) =>
+    fullScreen ? theme.colors.primaryLightBg : 'transparent'};
+  /* background-color: transparent; */
+`
+
+export const StyledSpinner = styled.svg`
+  animation: rotate 2s linear infinite;
+  margin: -25px 0 0 -25px;
+  width: 50px;
+  height: 50px;
+
+  & .path {
+    stroke: ${({ theme }) => theme.colors.primaryShade};
+    stroke-linecap: round;
+    animation: dash 1.5s ease-in-out infinite;
+  }
+
+  @keyframes rotate {
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+  @keyframes dash {
+    0% {
+      stroke-dasharray: 1, 150;
+      stroke-dashoffset: 0;
+    }
+    50% {
+      stroke-dasharray: 90, 150;
+      stroke-dashoffset: -35;
+    }
+    100% {
+      stroke-dasharray: 90, 150;
+      stroke-dashoffset: -124;
+    }
+  }
+`
